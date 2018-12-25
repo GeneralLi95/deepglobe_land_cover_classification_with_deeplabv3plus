d6524d76a4718fbe0291f8a4b5d61057336082a2
liyao <nudtliyao@foxmail.com>
2018-12-25 10:20:26 +0800

    update land_train.py

--- land_train.py
+++ land_train.py
@@ -5,11 +5,17 @@
 @file: land_train.py 
 @time: 2018/12/19 
 """
-import numpy as np
-import os
-import tensorflow as tf
 from utils.data_utils import DataSet, read_data
 from tensorflow.python.framework import ops
+from deeplab_v3 import Deeplab_v3
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+import os
+
 ops.reset_default_graph()
 
 class args:
@@ -33,11 +39,21 @@ valid_label_path = 'dataset/valid_label'
 
 
 train_img = read_data(train_sat_path)
-train_label = read_data(train_mask_path)
+train_label = read_data(train_label_path)
 valid_img = read_data(valid_sat_path)
-valid_label = read_data(valid_mask_path)
-print(train_img)
-print(train_label)
+valid_label = read_data(valid_label_path)
+# print(train_img)
+# print(train_label)
 dataset_tr = DataSet(image_path=train_img, label_path=train_label)
 dataset_val = DataSet(image_path=valid_img, label_path=valid_label)
 print('Data input success.')
+
+model = Deeplab_v3(batch_norm_decay=args.batch_norm_decay)
+
+image = tf.placeholder(tf.float32, [None, 2448, 2448, 3], name='input_x')
+label = tf.placeholder(tf.int32, [None, 2448, 2448])
+lr = tf.placeholder(tf.float32)
+
+logits = model.forward_pass(image)
+predicts = tf.argmax(logits, axis=-1, name='predicts')
+
